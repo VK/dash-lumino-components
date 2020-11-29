ce2b856d7313d5187ee0b76ddb3c69b41daec725
VK <viktor@krueckl.de>
2020-11-29 17:20:48 +0100

    release 0.0.4

--- setup.py
+++ setup.py
@@ -8,15 +8,21 @@ with open('package.json') as f:
 
 package_name = package["name"].replace(" ", "_").replace("-", "_")
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name=package_name,
+    url="https://github.com/VK/dash-lumino-components",
     version=package["version"],
     author=package['author'],
     packages=[package_name],
     include_package_data=True,
     license=package['license'],
     description=package.get('description', package_name),
-    install_requires=[],
+    long_description= long_description,
+    long_description_content_type="text/markdown",    
+    install_requires=[dash],
     classifiers = [
         'Framework :: Dash',
     ],    
